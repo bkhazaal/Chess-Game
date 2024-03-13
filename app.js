5f60b1b328ff39ba672420f862d5d7cbba6f5af0
bkhazaal <thajukez101@gmail.com>
2024-03-13 16:50:09 -0700

    chessbishh

---
+++ app.js
@@ -0,0 +1,5 @@
+const gameBoard = document.querySelector('#gameboard');
+const PlayerDisplay = document.querySelector("#player");
+const InfoDisplay = document.querySelector("#info-display");
+const width = 8
+const startPieces = []
