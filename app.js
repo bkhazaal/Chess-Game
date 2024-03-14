1a1de6f575e85f231f160ad2a926799cdb2c9be4
bkhazaal <thajukez101@gmail.com>
2024-03-14 16:43:27 -0700

    chess

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ function createBoard() {
     startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.innerHTML = startPiece;
         square.setAttribute('square-id', i);
-        square.classList.add('beige');
+        const row = Math.floor ((63-i)/8) + 1;
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown");
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige");
+        }
+
         gameBoard.append(square);
     })
 }
