b63891aa0967653edc49441bec1b58a2f1553bbe
bkhazaal <thajukez101@gmail.com>
2024-03-13 17:34:54 -0700

    board

--- app.js
+++ app.js
@@ -2,4 +2,26 @@ const gameBoard = document.querySelector('#gameboard');
 const PlayerDisplay = document.querySelector("#player");
 const InfoDisplay = document.querySelector("#info-display");
 const width = 8
-const startPieces = []
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    '', '', '', '', '', '', '', '', 
+    '', '', '', '', '', '', '', '', 
+    '', '', '', '', '', '', '', '', 
+    '', '', '', '', '', '', '', '', 
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+]
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.setAttribute('square-id', i);
+        square.classList.add('beige');
+        gameBoard.append(square);
+    })
+}
+
+createBoard();
