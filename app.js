409f9a284d40059cae3459cc7a2d8e660d99f337
bkhazaal <thajukez101@gmail.com>
2024-03-14 20:36:31 -0700

    yea

--- app.js
+++ app.js
@@ -22,12 +22,18 @@ function createBoard() {
         square.setAttribute('square-id', i);
         const row = Math.floor ((63-i)/8) + 1;
         if (row % 2 === 0) {
-            square.classList.add(i % 2 === 0 ? "beige" : "brown");
+            square.classList.add(i % 2 === 0 ? "gray" : "brown");
         } else {
-            square.classList.add(i % 2 === 0 ? "brown" : "beige");
+            square.classList.add(i % 2 === 0 ? "brown" : "gray");
         }
-        
+
         gameBoard.append(square);
+
+        if (i < 16) {
+            square.classList.add('black');
+        } else {
+            square.classList.add('white');
+        }
     })
 }
 
